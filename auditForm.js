0fca89a725c8294a63c76ad17c1d05ccc2b961f0
bullyliit <myxski@gmail.com>
2017-10-31 20:28:32 +0800

    dynamic adding of inputs for additional nakuha sa kaha

---
+++ auditForm.js
@@ -0,0 +1,23 @@
+var i = 0; /* Set Global Variable i */
+function increment(){
+    // Naming scheme for the input feilds. Appends the number to the feild's name
+    i+=1;
+}
+
+function addFeild(spanId){
+    var lst = document.createElement("li");
+    var lbl = document.createElement('label');
+    var inptWho = document.createElement("INPUT");
+    var inptHwMch = document.createElement("INPUT");
+    lbl.setAttribute("for", "nakuha_" + i);
+    inptWho.setAttribute("type", "text");
+    inptWho.setAttribute("name", "sino_" + i);
+    inptWho.setAttribute("placeholder", "Sinong nanguha");
+    inptHwMch.setAttribute("type", "number");
+    inptHwMch.setAttribute("name", "nakuha_" + i);
+    inptHwMch.setAttribute("placeholder", "Magkano nakuha");
+    lst.appendChild(lbl);
+    lst.appendChild(inptWho);
+    lst.appendChild(inptHwMch);
+    document.getElementById(spanId).appendChild(lst);
+}
